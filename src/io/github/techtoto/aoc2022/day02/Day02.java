5600011c396f0cff840340c36e9096e60084f81c
techtoto <toto.rei0210@gmail.com>
2022-12-03 00:47:22 +0100

    small improvements on day 2

--- src/io/github/techtoto/aoc2022/day02/Day02.java
+++ src/io/github/techtoto/aoc2022/day02/Day02.java
@@ -16,68 +16,67 @@ public class Day02 extends AbstractDay {
 
         input = FileInteraction.readFileToList(inputFile);
 
-        for (String play : input) {
-            String[] currentPlays = play.split(" ");
-            totalScore1 += rockPaperScissors1(currentPlays[0].charAt(0), currentPlays[1].charAt(0));
-            totalScore2 += rockPaperScissors2(currentPlays[0].charAt(0), currentPlays[1].charAt(0));
+        for (String card : input) {
+            totalScore1 += rockPaperScissors1(card.charAt(0), card.charAt(2));
+            totalScore2 += rockPaperScissors2(card.charAt(0), card.charAt(2));
         }
 
         setSolution(0, totalScore1);
         setSolution(1, totalScore2);
     }
 
-    private int rockPaperScissors1(char shape1, char shape2) {
-        if (shape1 == 'A') { // opponent chooses Rock
-            if (shape2 == 'X') { // you choose Rock (1) -> draw (3)
+    private int rockPaperScissors1(char card1, char card2) {
+        if (card1 == 'A') { // opponent chooses Rock
+            if (card2 == 'X') { // you choose Rock (1) -> draw (3)
                 return 4;
-            } else if (shape2 == 'Y') { // you choose Paper (2) -> win (6)
+            } else if (card2 == 'Y') { // you choose Paper (2) -> win (6)
                 return 8;
-            } else if (shape2 == 'Z') { // you choose Scissors (3) -> lose (0)
+            } else if (card2 == 'Z') { // you choose Scissors (3) -> lose (0)
                 return 3;
             }
-        } else if (shape1 == 'B') { // opponent chooses Paper
-            if (shape2 == 'X') { // you choose Rock (1) -> lose (0)
+        } else if (card1 == 'B') { // opponent chooses Paper
+            if (card2 == 'X') { // you choose Rock (1) -> lose (0)
                 return 1;
-            } else if (shape2 == 'Y') { // you choose Paper (2) -> draw (3)
+            } else if (card2 == 'Y') { // you choose Paper (2) -> draw (3)
                 return 5;
-            } else if (shape2 == 'Z') { // you choose Scissors (3) -> win (6)
+            } else if (card2 == 'Z') { // you choose Scissors (3) -> win (6)
                 return 9;
             }
-        } else if (shape1 == 'C') { // opponent chooses Scissors
-            if (shape2 == 'X') { // you choose Rock (1) -> win (6)
+        } else if (card1 == 'C') { // opponent chooses Scissors
+            if (card2 == 'X') { // you choose Rock (1) -> win (6)
                 return 7;
-            } else if (shape2 == 'Y') { // you choose Paper (2) -> lose (0)
+            } else if (card2 == 'Y') { // you choose Paper (2) -> lose (0)
                 return 2;
-            } else if (shape2 == 'Z') { // you choose Scissors (3) -> draw (3)
+            } else if (card2 == 'Z') { // you choose Scissors (3) -> draw (3)
                 return 6;
             }
         }
         return 0;
     }
 
-    private int rockPaperScissors2(char shape1, char shape2) {
-        if (shape1 == 'A') { // opponent chooses Rock
-            if (shape2 == 'X') { // you must lose (0) -> play scissors (3)
+    private int rockPaperScissors2(char card1, char card2) {
+        if (card1 == 'A') { // opponent chooses Rock
+            if (card2 == 'X') { // you must lose (0) -> play scissors (3)
                 return 3;
-            } else if (shape2 == 'Y') { // you must have a draw (3) -> play rock (1)
+            } else if (card2 == 'Y') { // you must have a draw (3) -> play rock (1)
                 return 4;
-            } else if (shape2 == 'Z') { // you must win (6) -> play paper (2)
+            } else if (card2 == 'Z') { // you must win (6) -> play paper (2)
                 return 8;
             }
-        } else if (shape1 == 'B') { // opponent chooses Paper
-            if (shape2 == 'X') { // you must lose (0) -> play rock (1)
+        } else if (card1 == 'B') { // opponent chooses Paper
+            if (card2 == 'X') { // you must lose (0) -> play rock (1)
                 return 1;
-            } else if (shape2 == 'Y') { // you must have a draw (3) -> play paper (2)
+            } else if (card2 == 'Y') { // you must have a draw (3) -> play paper (2)
                 return 5;
-            } else if (shape2 == 'Z') { // you must win (6) -> play scissors (3)
+            } else if (card2 == 'Z') { // you must win (6) -> play scissors (3)
                 return 9;
             }
-        } else if (shape1 == 'C') { // opponent chooses Scissors
-            if (shape2 == 'X') { // you must lose (0) -> play paper (2)
+        } else if (card1 == 'C') { // opponent chooses Scissors
+            if (card2 == 'X') { // you must lose (0) -> play paper (2)
                 return 2;
-            } else if (shape2 == 'Y') { // you must have a draw (3) -> play scissors (3)
+            } else if (card2 == 'Y') { // you must have a draw (3) -> play scissors (3)
                 return 6;
-            } else if (shape2 == 'Z') { // you must win (6) -> play rock (1)
+            } else if (card2 == 'Z') { // you must win (6) -> play rock (1)
                 return 7;
             }
         }
